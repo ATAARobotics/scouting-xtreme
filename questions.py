97e7f1080a6eae34004680f5a0d92e54f1ed00ab
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2024-10-08 20:35:05 -0600

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,2 +1,2 @@
-pitq = {'Team Information': {'Type': 'Header'}, 'Team No.': {'Type': 'Number Input', 'Minimum': 1, 'Maximum': 30000}, 'Present': {'Type': 'Multiple Choice', 'Options': ['No', 'Yes'], 'DefaultIndex': 0}}
-matchq = {'Round No.': {'Type': 'Number Input', 'Minimum': 1, 'Maximum': 100}, 'Team Information': {'Type': 'Header'}, 'Team No.': {'Type': 'Number Input', 'Minimum': 1, 'Maximum': 30000}, 'Notes:': {'Type': 'Text Input', 'Character Limit': 200}}
+pitq = {'Team Information': {'Type': 'Header'}, 'Team No.': {'Type': 'Number Input', 'Minimum': 1, 'Maximum': 30000}, 'Present:': {'Type': 'Multiple Choice', 'Options': ['No', 'Yes'], 'DefaultIndex': 0}}
+matchq = {'Round No.': {'Type': 'Number Input', 'Minimum': 1, 'Maximum': 100}, 'Team Information': {'Type': 'Header'}, 'Team No.': {'Type': 'Number Input', 'Minimum': 1, 'Maximum': 30000}, 'Present:': {'Type': 'Multiple Choice', 'Options': ['No', 'Yes'], 'DefaultIndex': 0}}
