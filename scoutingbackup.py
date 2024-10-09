7a16e6a3dbf54329ff015454e8c41114f4ebad9d
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2024-10-08 20:36:03 -0600

    Absence of this file caused an error, it has been readded

---
+++ scoutingbackup.py
@@ -0,0 +1,2 @@
+pitq = {'Team Information': {'Type': 'Header'}, 'Team No.': {'Type': 'Number Input', 'Minimum': 1, 'Maximum': 30000}, 'Present:': {'Type': 'Multiple Choice', 'Options': ['No', 'Yes'], 'DefaultIndex': 0}}
+matchq = {'Round No.': {'Type': 'Number Input', 'Minimum': 1, 'Maximum': 100}, 'Team Information': {'Type': 'Header'}, 'Team No.': {'Type': 'Number Input', 'Minimum': 1, 'Maximum': 30000}, 'Present:': {'Type': 'Multiple Choice', 'Options': ['No', 'Yes'], 'DefaultIndex': 0}}
