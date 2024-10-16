96d7f84232dfe0604bf0a6ee3261e50dd4c28fa1
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2024-10-15 20:14:35 -0600

    Update scoutingbackup.py

--- scoutingbackup.py
+++ scoutingbackup.py
@@ -1,2 +1,4 @@
-pitq = {'Team Information': {'Type': 'Header'}, 'Team No.': {'Type': 'Number Input', 'Minimum': 1, 'Maximum': 30000}, 'Present:': {'Type': 'Multiple Choice', 'Options': ['No', 'Yes'], 'DefaultIndex': 0}}
-matchq = {'Round No.': {'Type': 'Number Input', 'Minimum': 1, 'Maximum': 100}, 'Team Information': {'Type': 'Header'}, 'Team No.': {'Type': 'Number Input', 'Minimum': 1, 'Maximum': 30000}, 'Present:': {'Type': 'Multiple Choice', 'Options': ['No', 'Yes'], 'DefaultIndex': 0}}
+
+pitdata = {'Team No.': ['4421', '1', '1', '1', '4334'], 'Notes:': ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], 'Points Scored:': ['N/A', 'N/A', 'N/A', 'N/A', 'N/A']}
+matchdata = {'Round No.': ['0', '1', '23'], 'Team No.': ['4421', '1', '6082'], 'Present:': ['N/A', 'N/A', 'N/A']}
+    
