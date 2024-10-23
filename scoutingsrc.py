7e111592033afed97baf651e82d6ba385ba9eed8
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2024-10-22 19:56:47 -0600

    Update scoutingsrc.py

--- scoutingsrc.py
+++ scoutingsrc.py
@@ -1,3 +1,3 @@
 
-pitdata = {'Team No.': ['4421', '1', '1', '1', '4334'], 'Notes:': ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], 'Points Scored:': ['N/A', 'N/A', 'N/A', 'N/A', 'N/A']}
-matchdata = {'Round No.': ['0', '1', '23'], 'Team No.': ['4421', '1', '6082'], 'Present:': ['N/A', 'N/A', 'N/A']}
+pitdata = {'Team No.': ['4421', '1', '1', '1', '4334'], 'Drive Method:': ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], 'Notes:': ['nan', 'nan', 'nan', 'nan', 'nan']}
+matchdata = {'Round No.': ['0', '1', '23'], 'Team No.': ['4421', '1', '6082'], 'Present:': ['N/A', 'N/A', 'N/A'], 'Drive Method': ['N/A', 'N/A', 'N/A']}
