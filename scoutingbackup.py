94f02fc2b12224c9745d22b5a9b84674067fe248
AwesomedudeX <technodudea1@gmail.com>
2024-11-02 16:15:37 -0600

    Update GitHub

--- scoutingbackup.py
+++ scoutingbackup.py
@@ -1,4 +1,4 @@
 
-pitdata = {'Team No.': ['4421', '1', '1', '1', '4334', 'Swerve Drive'], 'Drive Method:': ['N/A', 'N/A', 'N/A', 'N/A', 'N/A', '1'], 'Notes:': ['nan', 'nan', 'nan', 'nan', 'nan', '']}
-matchdata = {'Round No.': ['0', '1', '23'], 'Team No.': ['4421', '1', '6082'], 'Present:': ['N/A', 'N/A', 'N/A'], 'Drive Method': ['N/A', 'N/A', 'N/A']}
+pitdata = {'Team No.': [], 'Drive Method:': [], 'Notes:': [], 'Team Information': [], 'Other Information': []}
+matchdata = {'Round No.': [], 'Team No.': [], 'Present:': [], 'Drive Method': [], 'Team Information': []}
     
--- scoutingsrc.py
+++ scoutingsrc.py
@@ -1,3 +1,3 @@
 
-pitdata = {'Team No.': ['4421', '1', '1', '1', '4334', 'Swerve Drive', 'Tank Drive', 'Swerve Drive'], 'Drive Method:': ['N/A', 'N/A', 'N/A', 'N/A', 'N/A', '1', '1', '1'], 'Notes:': ['nan', 'nan', 'nan', 'nan', 'nan', '', '', '']}
-matchdata = {'Round No.': ['0', '1', '23'], 'Team No.': ['4421', '1', '6082'], 'Present:': ['N/A', 'N/A', 'N/A'], 'Drive Method': ['N/A', 'N/A', 'N/A']}
+pitdata = {'Team No.': [], 'Drive Method:': [], 'Notes:': [], 'Team Information': [], 'Other Information': []}
+matchdata = {'Round No.': [], 'Team No.': [], 'Present:': [], 'Drive Method': [], 'Team Information': []}
