2aab0fcf63ca397372659bd923504c95168af28e
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2024-11-09 14:31:54 -0700

    Update scoutingsrc.py

--- scoutingsrc.py
+++ scoutingsrc.py
@@ -1,3 +1,3 @@
 
-pitdata = {'Team No.': [], 'Drive Method:': [], 'Notes:': []}
+pitdata = {'Team No.': [4421], 'Drive Method:': ["Swerve Drive"], 'Notes:': ["Extraordinary team"]}
 matchdata = {'Round No.': [], 'Team No.': [], 'Present:': [], 'Drive Method': []}
