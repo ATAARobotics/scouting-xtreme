7d906c4338eb2370ba8fb3225f537a4462eeb733
Jason Heard <jasonpheard@101100.ca>
2024-11-16 13:58:13 -0700

    Add minio test files

---
+++ tests/download.py
@@ -0,0 +1,23 @@
+from io import BytesIO
+from minio import Minio
+
+def load_csv():
+    client = Minio("minio.101100.ca",
+        access_key="uyBquJD1wBqebVcMhIgq",
+        secret_key="ZGa6YUerybM1Jd0NPYugZhTnPhbtb48cZLiFuKYo",
+    )
+
+    try:
+        response = client.get_object("scouting-xtreme", "data.csv")
+
+        return response.data.decode("utf-8")
+    except ResponseError as err:
+        print("Failed to upload with error:", err)
+    finally:
+        response.close()
+        response.release_conn()
+
+
+loaded_csv = load_csv()
+
+print("Got CSV:", loaded_csv)
---
+++ tests/upload.py
@@ -0,0 +1,24 @@
+from io import BytesIO
+from minio import Minio
+
+def save_csv(csv_string):
+    client = Minio("minio.101100.ca",
+        access_key="uyBquJD1wBqebVcMhIgq",
+        secret_key="ZGa6YUerybM1Jd0NPYugZhTnPhbtb48cZLiFuKYo",
+    )
+
+    csv_bytes = csv_string.encode('utf-8')
+    try:
+        result = client.put_object(
+            "scouting-xtreme", "data.csv", BytesIO(csv_bytes), len(csv_bytes),
+            content_type="application/csv",
+        )
+    except ResponseError as err:
+        print("Failed to upload with error:", err)
+
+
+save_csv("""Col1,Col2,Col3
+1,2,3
+4,5,7
+7,8,9
+""")
